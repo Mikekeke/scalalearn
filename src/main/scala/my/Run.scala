ff58e8340a78419dcb453bd647c601162d310cd0
Mikhel <m282021@gmail.com>
2017-02-28 14:04:16 +0300

    23

--- src/main/scala/my/Run.scala
+++ src/main/scala/my/Run.scala
@@ -1,5 +1,7 @@
 package my
 
+import my.pattern_matching.{Apple, Fruit, Orange, appleLil}
+
 /**
   * Created by ibes on 22.02.17.
   */
@@ -7,5 +9,18 @@ object Run {
   def main(args: Array[String]): Unit = {
     val ints = MyArgs("one", "two", "3")
     println(ints)
+
+    // pattern matching tests (doesn't work in REPL for some reason)
+    def testFruit(f: Fruit) = f match {
+      case o @ appleLil() => println(o)
+      case a @ Apple("Frank") => println("This is apple Frank!")
+      case Apple(name) => println(s"Apple name: $name")
+      case or: Orange => println(or)
+    }
+
+    testFruit(Apple("Boris"))
+    testFruit(Orange("Tom"))
+    testFruit(Apple("Lil"))
+    testFruit(Apple("Frank"))
   }
 }
