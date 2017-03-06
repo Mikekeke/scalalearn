e31b8af3b2be25e7fab0f5f60863377d762ca027
m282021@gmail.com <15197delJ>
2017-03-06 13:32:59 +0300

    ...

--- src/main/scala/my/FunctionReturningFunction.scala
+++ src/main/scala/my/FunctionReturningFunction.scala
@@ -9,9 +9,11 @@ object FunctionReturningFunction extends App {
 
   def plusTen: (Int) => Int = incrementer(10)
 
-    val ex_one = plusTen(3)
+  val ex_one = plusTen(3)
   //calling 2 functions one line
   val ex_two: Int = incrementer(5)(3)
+
+
   println(s"ex_one = $ex_one")
   println(s"ex_two = $ex_two")
 
