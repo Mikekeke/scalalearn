f538d43574734cbe3eacc1bf3035e6717af6949d
m282021@gmail.com <15197delJ>
2017-03-13 16:41:31 +0300

    Option

--- src/main/scala/streams/StreamRun.scala
+++ src/main/scala/streams/StreamRun.scala
@@ -5,7 +5,8 @@ package streams
   */
 object StreamRun extends App{
   val stream = Stream(1,2,3,4,5)
-  println(stream.toList)
-  println(stream.take(3).toList)
-  println(stream.drop(0).toList)
+  println(s"To list: ${stream.toList}")
+  println(s"Take 3: ${stream.take(3).toList}")
+  println(s"Drop 3: ${stream.drop(3).toList}")
+  println(s"Take wile: ${stream.takeWhile(_ < 4).toList}")
 }
