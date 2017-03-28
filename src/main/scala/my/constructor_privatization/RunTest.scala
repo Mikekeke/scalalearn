71155aedc988fa0564487637e6a283fca8b5447e
m282021@gmail.com <15197delJ>
2017-03-28 13:52:35 +0300

    ...

--- src/main/scala/my/constructor_privatization/RunTest.scala
+++ src/main/scala/my/constructor_privatization/RunTest.scala
@@ -19,6 +19,5 @@ object RunTest extends App {
   // ain't work coz default constructor is private
 //  val nErr = new RegularPrivate(3, "test")
 //  println(nErr)
-val cleanStringWith3: String => (String => String) = s => fun => fun()
 
 }
