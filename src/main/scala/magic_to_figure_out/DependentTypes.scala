80635044a51e7e559b1ff8f02d736937621b030b
Mikhel <m282021@gmail.com>
2017-08-12 20:44:32 +0300

    ...

--- src/main/scala/magic_to_figure_out/DependentTypes.scala
+++ src/main/scala/magic_to_figure_out/DependentTypes.scala
@@ -29,3 +29,34 @@ object DependentTypes extends App{
   testLin.possibleOnlyWithLinux(new Linux2)
 //  testLin.possibleOnlyWithLinux(new Windows2)  //won't compile
 }
+
+//  A:
+//  trait Pet
+//  class Cat extends Pet
+//
+//  class Person {
+//    type Pet
+//  }
+//
+//  class Susan extends Person {
+//    type Pet = Cat
+//  }
+//
+//  ----------------------------------------------------------
+//
+//  B:
+//  trait Pet
+//  class Cat extends Pet
+//
+//  class Person[Pet]
+//
+//  class Susan extends Person[Cat]
+//
+//  http://typelevel.org/blog/2015/07/13/type-members-parameters.html
+//
+//  Oleg Nizhnik
+//  В scala 2 это пока очень разные вещи.
+//  1. type member ы не могут быть ко\контр вариантными
+//  2. type member ы лучше работают как type families аналог, если совмещать их с typeclass ами, компилятор всегда будет стараться вывести и сравнить их, а не начинать с них поиск имплиситов
+//  3. В type parameters вы не можете сослаться на type members, а наоборот - можно
+//  В dotty эту разницу нивелируют и type parameters будет синтаксическим сахаром для type members
