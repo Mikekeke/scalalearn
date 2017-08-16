3c17876b424bcf8d22680e6cb9708aaf7548dfb2
m282021@gmail.com <zDmVJe6D>
2017-08-16 13:18:42 +0300

    Actors tests

--- build.sbt
+++ build.sbt
@@ -4,4 +4,10 @@ version := "1.0"
 
 scalaVersion := "2.12.3"
 scalacOptions += "-feature"
+
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.5.4",
+  "com.typesafe.akka" %% "akka-testkit" % "2.5.4" % Test
+)
     
---
+++ src/main/scala/my/AkkaActorsTests.scala
@@ -0,0 +1,50 @@
+package my
+
+import java.util.Calendar
+import java.util.concurrent.{ExecutorService, Executors}
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+case object Run
+
+trait Blocking {
+  def block(name: String) = {
+    Thread.sleep(3000)
+    println(s"$name - Block done")
+  }
+}
+
+class BlockedActor extends Actor with Blocking {
+  val name = this.getClass.getSimpleName
+
+  override def receive = {
+    case r@Run =>
+      println(s"$name - $r at ${Calendar.getInstance().getTime}")
+      block(name)
+  }
+}
+
+class NonBlockedActor extends Actor with Blocking {
+  val name = this.getClass.getSimpleName
+  implicit val futuresContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(4))
+
+  override def receive = {
+    case r@Run =>
+      println(s"$name - $r at ${Calendar.getInstance().getTime}")
+      Future(block(name))
+  }
+}
+
+object AkkaActorsTests extends App {
+  val system = ActorSystem("test")
+
+  val blocked = system.actorOf(Props[BlockedActor])
+  val nonBlocked = system.actorOf(Props[NonBlockedActor])
+
+  blocked ! Run
+  blocked ! Run
+  nonBlocked ! Run
+  nonBlocked ! Run
+}
