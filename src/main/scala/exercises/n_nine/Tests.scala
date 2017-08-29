c382370e54180f73be75b56fffd2119a621d8071
m282021@gmail.com <zDmVJe6D>
2017-08-29 17:52:33 +0300

    n_nines - more

--- src/main/scala/exercises/n_nine/Tests.scala
+++ src/main/scala/exercises/n_nine/Tests.scala
@@ -1,7 +1,9 @@
 package exercises.n_nine
+import exercises.n_nine.Tasks._
 
 object Tests extends App{
   val ls = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
-//  List('d, 'e, 'f, 'g)
-  println(ls.slice(40,1))
+
+
+  println(myRotate1(3, ls).zip(List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c)).forall {case (a, b) => a == b})
 }
